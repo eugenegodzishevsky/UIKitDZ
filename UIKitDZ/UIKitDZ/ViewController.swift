0b339e51a24e0054b384d670200d847e0fe3746f
Vermut xxx <>
2024-02-06 14:08:20 +1000

    initial commit

---
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  UIKitDZ
+//
+//  Created by Vermut xxx on 06.02.2024.
+//
+
+import UIKit
+/// класс
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}

b4a830be1a2460f934f75ae3e3c4317947440c50
Vermut xxx <>
2024-02-12 23:18:57 +1000

    fixed

--- UIKitDZ/UIKitDZ/NewReminderViewController.swift
+++ UIKitDZ/UIKitDZ/NewReminderViewController.swift
@@ -6,7 +6,7 @@ import UIKit
 /// Вью контроллер для добавления нового напоминания
 final class NewReminderViewController: UIViewController {
     // MARK: - Private Properties
-    
+
     private var dateOfBirthField: UITextField?
     private var ageField: UITextField?
     private var agePicker: UIPickerView?
@@ -14,25 +14,25 @@ final class NewReminderViewController: UIViewController {
     private var genderPicker: UIPickerView?
     private let genders = ["Male", "Female"]
     private var telegramField: UITextField?
-    
+
     // MARK: - Life Cycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         addCancelButton()
-        addAddButton()
+        addNewBirthdayButton()
         addPhotoPic()
         addChangePhotoButton()
         addNames()
         addTextfields()
         setupLines()
-        
+
         telegramField?.delegate = self
     }
-    
+
     // MARK: - Private Methods
-    
+
     private func addCancelButton() {
         let cancelButton = UIButton(frame: CGRect(x: 20, y: 20, width: 50, height: 20))
         cancelButton.setTitle("Cancel", for: .normal)
@@ -40,21 +40,21 @@ final class NewReminderViewController: UIViewController {
         cancelButton.setTitleColor(.systemBlue, for: .normal)
         view.addSubview(cancelButton)
     }
-    
-    private func addAddButton() {
+
+    private func addNewBirthdayButton() {
         let addButton = UIButton(frame: CGRect(x: 315, y: 20, width: 50, height: 20))
         addButton.setTitle("Add", for: .normal)
         addButton.titleLabel?.font = UIFont(name: "Verdana", size: 14)
         addButton.setTitleColor(.systemBlue, for: .normal)
         view.addSubview(addButton)
     }
-    
+
     private func addPhotoPic() {
         let photoPic = UIImageView(frame: CGRect(x: 125, y: 134, width: 125, height: 125))
         photoPic.image = UIImage(named: "photoPic")
         view.addSubview(photoPic)
     }
-    
+
     private func addChangePhotoButton() {
         let changePhotoButton = UIButton(frame: CGRect(x: 125, y: 267, width: 125, height: 20))
         changePhotoButton.setTitle("Change photo", for: .normal)
@@ -62,7 +62,7 @@ final class NewReminderViewController: UIViewController {
         changePhotoButton.setTitleColor(.systemBlue, for: .normal)
         view.addSubview(changePhotoButton)
     }
-    
+
     // методы для установки заголовков
     private func addHeaderLabel(yCoordinate: CGFloat, text: String) {
         let nameLabel = UILabel(frame: CGRect(x: 20, y: yCoordinate, width: 175, height: 19))
@@ -71,7 +71,7 @@ final class NewReminderViewController: UIViewController {
         nameLabel.font = UIFont.boldSystemFont(ofSize: 16)
         view.addSubview(nameLabel)
     }
-    
+
     private func addNames() {
         addHeaderLabel(yCoordinate: 307, text: "Name Surname")
         addHeaderLabel(yCoordinate: 382, text: "Birthday")
@@ -79,7 +79,7 @@ final class NewReminderViewController: UIViewController {
         addHeaderLabel(yCoordinate: 530, text: "Gender")
         addHeaderLabel(yCoordinate: 605, text: "Telegram")
     }
-    
+
     // методы для установки текстфилдов
     private func addTextFieldLabel(yCoordinate: CGFloat, placeholder: String) -> UITextField {
         let textFieldLabel = UITextField(frame: CGRect(x: 20, y: yCoordinate, width: 175, height: 19))
@@ -89,7 +89,7 @@ final class NewReminderViewController: UIViewController {
         view.addSubview(textFieldLabel)
         return textFieldLabel
     }
-    
+
     // методы для создания нижних подчеркиваний
     private func setupUnderLine(yCoordinate: CGFloat) -> UILabel {
         let line = UILabel(frame: CGRect(x: 20, y: yCoordinate, width: 335, height: 1))
@@ -97,7 +97,7 @@ final class NewReminderViewController: UIViewController {
         view.addSubview(line)
         return line
     }
-    
+
     private func setupLines() {
         _ = setupUnderLine(yCoordinate: 361)
         _ = setupUnderLine(yCoordinate: 436)
@@ -105,7 +105,7 @@ final class NewReminderViewController: UIViewController {
         _ = setupUnderLine(yCoordinate: 584)
         _ = setupUnderLine(yCoordinate: 659)
     }
-    
+
     private func addTextfields() {
         _ = addTextFieldLabel(yCoordinate: 336, placeholder: "Typing Name Surname")
         dateOfBirthField = addTextFieldLabel(yCoordinate: 411, placeholder: "Typing Date of Birth")
@@ -116,10 +116,10 @@ final class NewReminderViewController: UIViewController {
         datePicker.backgroundColor = .white
         datePicker.addTarget(self, action: #selector(dateChanged(_:)), for: .valueChanged)
         dateOfBirthField?.inputView = datePicker
-        
+
         let birthDayToolbar = UIToolbar()
         birthDayToolbar.sizeToFit()
-        
+
         let birthDayDoneButton = UIBarButtonItem(
             barButtonSystemItem: .done,
             target: self,
@@ -128,7 +128,7 @@ final class NewReminderViewController: UIViewController {
         let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         birthDayToolbar.setItems([flexSpace, birthDayDoneButton], animated: true)
         dateOfBirthField?.inputAccessoryView = birthDayToolbar
-        
+
         ageField = addTextFieldLabel(yCoordinate: 486, placeholder: "Typing age")
         // пикер количества лет
         agePicker = UIPickerView()
@@ -136,14 +136,14 @@ final class NewReminderViewController: UIViewController {
         agePicker?.delegate = self
         agePicker?.dataSource = self
         ageField?.inputView = agePicker
-        
+
         let ageToolbar = UIToolbar()
         ageToolbar.sizeToFit()
-        
+
         let ageDoneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(ageDonePressed))
         ageToolbar.setItems([flexSpace, ageDoneButton], animated: true)
         ageField?.inputAccessoryView = ageToolbar
-        
+
         genderField = addTextFieldLabel(yCoordinate: 559, placeholder: "Indicate Gender")
         // пикер пола
         genderPicker = UIPickerView()
@@ -151,10 +151,10 @@ final class NewReminderViewController: UIViewController {
         genderPicker?.delegate = self
         genderPicker?.dataSource = self
         genderField?.inputView = genderPicker
-        
+
         let genderToolbar = UIToolbar()
         genderToolbar.sizeToFit()
-        
+
         let genderDoneButton = UIBarButtonItem(
             barButtonSystemItem: .done,
             target: self,
@@ -163,28 +163,28 @@ final class NewReminderViewController: UIViewController {
         // let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         genderToolbar.setItems([flexSpace, genderDoneButton], animated: true)
         genderField?.inputAccessoryView = genderToolbar
-        
+
         telegramField = addTextFieldLabel(yCoordinate: 634, placeholder: "Typing Telegram")
     }
-    
-    @objc func dateChanged(_ sender: UIDatePicker) {
+
+    @objc private func dateChanged(_ sender: UIDatePicker) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = .medium
         dateFormatter.timeStyle = .none
         dateOfBirthField?.text = dateFormatter.string(from: sender.date)
     }
-    
-    @objc func donePressed() {
+
+    @objc private func donePressed() {
         view.endEditing(true)
     }
-    
-    @objc func ageDonePressed() {
+
+    @objc private func ageDonePressed() {
         let selectedRow = agePicker?.selectedRow(inComponent: 0) ?? 0
         ageField?.text = "\(selectedRow)"
         view.endEditing(true)
     }
-    
-    @objc func genderDonePressed() {
+
+    @objc private func genderDonePressed() {
         let selectedRow = genderPicker?.selectedRow(inComponent: 0) ?? 0
         genderField?.text = genders[selectedRow]
         view.endEditing(true)
@@ -197,7 +197,7 @@ extension NewReminderViewController: UIPickerViewDataSource {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         1
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         if pickerView == agePicker {
             return 100
@@ -226,7 +226,7 @@ extension NewReminderViewController: UIPickerViewDelegate {
 extension NewReminderViewController: UITextFieldDelegate {
     func textFieldDidBeginEditing(_ textField: UITextField) {
         print("textFieldDidBeginEditing called")
-        
+
         if textField == telegramField {
             let alertController = UIAlertController(
                 title: "Please enter Telegram",
--- UIKitDZ/UIKitDZ/RegistrationViewController.swift
+++ UIKitDZ/UIKitDZ/RegistrationViewController.swift
@@ -6,15 +6,15 @@ import UIKit
 /// вью контроллер страницы регистрации
 final class RegistrationViewController: UIViewController {
     // MARK: - Private Properties
-    
+
     private var emailTextField: UITextField!
     private var passwordTextField: UITextField!
     private var loginButton: UIButton!
     private var faceIDLabel: UILabel!
     private var faceIDSwitch: UISwitch!
-    
+
     // MARK: - Life Cycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -30,20 +30,20 @@ final class RegistrationViewController: UIViewController {
         setupEyeImage()
         setupFaceIDLabel()
         setupFaceIDSwitch()
-        
+
         emailTextField?.addTarget(self, action: #selector(editingChanged), for: .editingChanged)
         passwordTextField?.addTarget(self, action: #selector(editingChanged), for: .editingChanged)
         loginButton?.addTarget(self, action: #selector(loginTapped), for: .touchUpInside)
     }
-    
+
     // MARK: - Private Methods
-    
+
     private func setupCakeImage() {
         let image = UIImageView(frame: CGRect(x: 125, y: 70, width: 125, height: 125))
         image.image = .init(named: "cake")
         view.addSubview(image)
     }
-    
+
     private func setupReminderLabel() {
         let label = UILabel(frame: CGRect(x: 100, y: 200, width: 175, height: 44))
         label.text = "Birthday\nReminder"
@@ -58,7 +58,7 @@ final class RegistrationViewController: UIViewController {
         )
         view.addSubview(label)
     }
-    
+
     private func setupSignInLabel() {
         let label = UILabel(frame: CGRect(x: 20, y: 256, width: 195, height: 51))
         label.text = "Sign in"
@@ -71,7 +71,7 @@ final class RegistrationViewController: UIViewController {
         )
         view.addSubview(label)
     }
-    
+
     private func setupEmailLabel() {
         let label = UILabel(frame: CGRect(x: 20, y: 318, width: 175, height: 19))
         label.text = "Email"
@@ -84,7 +84,7 @@ final class RegistrationViewController: UIViewController {
         )
         view.addSubview(label)
     }
-    
+
     private func setupPasswordlLabel() {
         let label = UILabel(frame: CGRect(x: 20, y: 393, width: 175, height: 19))
         label.text = "Password"
@@ -97,31 +97,31 @@ final class RegistrationViewController: UIViewController {
         )
         view.addSubview(label)
     }
-    
+
     private func setupEmailTextField() {
         emailTextField = UITextField(frame: CGRect(x: 20, y: 347, width: 175, height: 17))
         emailTextField.placeholder = "Typing email"
         view.addSubview(emailTextField)
     }
-    
+
     private func setupPasswordTextField() {
         passwordTextField = UITextField(frame: CGRect(x: 20, y: 422, width: 129, height: 17))
         passwordTextField.placeholder = "Typing password"
         view.addSubview(passwordTextField)
     }
-    
+
     private func setupUnderLine(yCoordinate: CGFloat) -> UILabel {
         let line = UILabel(frame: CGRect(x: 20, y: yCoordinate, width: 335, height: 1))
         line.backgroundColor = .lightGray
         view.addSubview(line)
         return line
     }
-    
+
     private func setupLines() {
         _ = setupUnderLine(yCoordinate: 372)
         _ = setupUnderLine(yCoordinate: 448)
     }
-    
+
     private func setupLoginButton() {
         loginButton = UIButton(frame: CGRect(x: 20, y: 671, width: 335, height: 44))
         loginButton.backgroundColor = UIColor(red: 233.0 / 255.0, green: 70.0 / 255.0, blue: 94.0 / 255.0, alpha: 0.4)
@@ -130,13 +130,13 @@ final class RegistrationViewController: UIViewController {
         loginButton.layer.cornerRadius = 12
         view.addSubview(loginButton)
     }
-    
+
     private func setupEyeImage() {
         let image = UIImageView(frame: CGRect(x: 331, y: 416, width: 24, height: 24))
         image.image = .init(named: "eye")
         view.addSubview(image)
     }
-    
+
     private func setupFaceIDLabel() {
         faceIDLabel = UILabel(frame: CGRect(x: 86, y: 544, width: 150, height: 35))
         faceIDLabel.text = "Use FaceID"
@@ -144,13 +144,13 @@ final class RegistrationViewController: UIViewController {
         faceIDLabel.isHidden = true
         view.addSubview(faceIDLabel)
     }
-    
+
     private func setupFaceIDSwitch() {
         faceIDSwitch = UISwitch(frame: CGRect(x: 248, y: 544, width: 54, height: 35))
         faceIDSwitch.isHidden = true
         view.addSubview(faceIDSwitch)
     }
-    
+
     @objc private func editingChanged(_ textField: UITextField) {
         guard let email = emailTextField.text,
               !email.isEmpty,
@@ -171,7 +171,7 @@ final class RegistrationViewController: UIViewController {
         faceIDLabel.isHidden = false
         faceIDSwitch.isHidden = false
     }
-    
+
     @objc private func loginTapped() {
         let birthdaysVC = BirthdaysViewController()
         birthdaysVC.modalPresentationStyle = .fullScreen
